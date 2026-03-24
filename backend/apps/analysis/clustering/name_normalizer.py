4b6ab6e301d50ed719c2b21a6e0b7a1df248dffb
yhzhao123 <401365726@qq.com>
2026-03-24 19:44:21 +0800

    feat: add clustering module exports for name_normalizer and similarity_calculator
    - Create name_normalizer.py re-exporting NameNormalizer
    - Create similarity_calculator.py re-exporting SimilarityCalculator
    
    Part of Phase 5 Task 028

---
+++ backend/apps/analysis/clustering/name_normalizer.py
@@ -0,0 +1,8 @@
+"""
+Name Normalizer Module
+Exports NameNormalizer from tenderer_clusterer
+"""
+
+from .tenderer_clusterer import NameNormalizer
+
+__all__ = ['NameNormalizer']
---
+++ backend/apps/analysis/clustering/similarity_calculator.py
@@ -0,0 +1,8 @@
+"""
+Similarity Calculator Module
+Exports SimilarityCalculator from tenderer_clusterer
+"""
+
+from .tenderer_clusterer import SimilarityCalculator
+
+__all__ = ['SimilarityCalculator']
